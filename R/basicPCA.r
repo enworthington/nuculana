2438dc0aa2abd966eac69cb767a9fe88192d9c4d
Robyn Dahl <34456426+robyndahl@users.noreply.github.com>
2020-03-04 10:41:39 -0800

    Create basicPCA.r

---
+++ R/basicPCA.r
@@ -0,0 +1,29 @@
+## Load geomorph package
+
+library(geomorph)
+
+## Load data
+
+arcs <- readland.tps("arcs.tps", specID = "ID")
+
+## Conduct GPA
+
+arcsGPA <- gpagen(arcs)
+
+arcsGPA
+
+plot(arcsGPA)
+
+## Conduct PCA
+
+arcPCA <- plotTangentSpace(arcsGPA$coords)
+
+arcsPCA$pc.summary
+
+## Color code by species
+
+specimenlist <- read.csv("specimenlist.csv", header = TRUE)
+
+plotTangentSpace(arcsGPA$coords, 
+                 groups = specimenlist$Species,
+                 legend = TRUE)
